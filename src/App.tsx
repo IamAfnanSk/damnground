b2f354b68ea0a2f23e0f58a0669baf0b95fcbbc5
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2021-06-08 01:15:41 +0530

    Made basic starter with ts

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
